268890573949b0f012ae0278f45d17fe933b7cc2
Stefan Blos <stefan.blos@gmail.com>
2019-12-02 11:41:11 +0100

    (Day 1) solved challenge 2

--- Day01/day01.py
+++ Day01/day01.py
@@ -4,9 +4,22 @@ def read_input():
         module_masses = [int(x.strip()) for x in f if x is not None]
     return module_masses
 
+def calculate_single_requirement(mass):
+    return mass // 3 - 2
+
+def compute_additional_fuel(module):
+    fuel_required = calculate_single_requirement(module)
+    if fuel_required <= 0:
+        return 0
+    else:
+        return fuel_required + compute_additional_fuel(fuel_required) 
+
 def compute_fuel_requirements(module_masses):
-    return sum([x // 3 - 2 for x in module_masses])
+    module_requirements = [calculate_single_requirement(x) for x in module_masses]
+    for index, module in enumerate(module_requirements):
+        module_requirements[index] += compute_additional_fuel(module)
+    return sum(module_requirements)
 
 module_masses = read_input()
 fuel_requirements = compute_fuel_requirements(module_masses)
-print(f"The fuel requirements are: '{fuel_requirements}'")
+print(f"The fuel requirements are: {fuel_requirements}.")
