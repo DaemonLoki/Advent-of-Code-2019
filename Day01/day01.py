a73ea4f57c70ac0b36d39df42e0482b5cbaa6e11
Stefan Blos <stefan.blos@gmail.com>
2019-12-02 11:30:07 +0100

    (Day 1) solved challenge 1

--- Day01/day01.py
+++ Day01/day01.py
@@ -1,8 +1,12 @@
 # Challenges from https://adventofcode.com/2019/day/1
 def read_input():
-    with open("day01.txt", "r") as f:
-        module_masses = [x.strip() for x in f if x is not None]
+    with open("Day01/day01.txt", "r") as f:
+        module_masses = [int(x.strip()) for x in f if x is not None]
     return module_masses
 
+def compute_fuel_requirements(module_masses):
+    return sum([x // 3 - 2 for x in module_masses])
+
 module_masses = read_input()
-print(module_masses)
+fuel_requirements = compute_fuel_requirements(module_masses)
+print(f"The fuel requirements are: '{fuel_requirements}'")
