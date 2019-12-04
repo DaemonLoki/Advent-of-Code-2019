1f2ccb857c377799b2f185665348428aca009824
Stefan Blos <stefan.blos@gmail.com>
2019-12-04 16:24:57 +0100

    (Day 4) solved challenge 2

--- Day04/day04.py
+++ Day04/day04.py
@@ -5,7 +5,7 @@ def read_input(file_name="Day04/day04.txt"):
         return map(int, f.readline().split("-"))
 
 def check_if_valid_password(num):
-    two_adjacent_digits, prev_digit = False, None
+    two_adjacent_digits, prev_digit, currently_matching, match_count = False, None, False, 0
     while num > 0:
         digit, num = num % 10, num // 10
         if prev_digit is None:
@@ -14,7 +14,16 @@ def check_if_valid_password(num):
     
         # check for adjacent digits
         if digit == prev_digit:
-            two_adjacent_digits = True
+            if currently_matching == False:
+                match_count = 2
+            else:
+                match_count += 1
+            currently_matching = True
+        else: 
+            if match_count == 2:
+                two_adjacent_digits = True
+            match_count = 0
+            currently_matching = False
 
         # check for not decreasing
         if digit > prev_digit:
@@ -22,6 +31,9 @@ def check_if_valid_password(num):
 
         # set digit to prev_digit
         prev_digit = digit
+
+    if match_count == 2:
+        two_adjacent_digits = True
     
     return two_adjacent_digits
 
@@ -31,13 +43,19 @@ def run_input_tests():
     assert stop == 748759
 
 def check_valid_password_tests():
-    assert check_if_valid_password(111111)
+    assert check_if_valid_password(111111) == False
     assert check_if_valid_password(223450) == False
     assert check_if_valid_password(123789) == False
+    assert check_if_valid_password(112233)
+    assert check_if_valid_password(123444) == False
+    assert check_if_valid_password(111122)
+    assert check_if_valid_password(112222)
  
 def run_tests():
     run_input_tests()
     check_valid_password_tests()
+
+    print("All tests passed.")
 
 run_tests()
 
