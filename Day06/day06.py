c8548596d5f98213c675c95f38a19186ab70fe3d
Stefan Blos <stefan.blos@gmail.com>
2019-12-08 15:52:18 +0100

    (Day 6) finished challenge 2

--- Day06/day06.py
+++ Day06/day06.py
@@ -28,12 +28,34 @@ def get_orbit_counts(orbit_map):
         orbit_counts[planet] = current_orbits
     return orbit_counts
 
-def solve_puzzle(test=False):
+def find_number_of_orbits(test=False):
     orbits = read_input(filename="Day06/day06_test.txt") if test else read_input()
     orbit_map = get_orbit_map(orbits)
     
     orbit_counts = get_orbit_counts(orbit_map)
-    return sum(orbit_counts.values())
+    return sum(orbit_counts.values()), orbit_map
+
+def find_min_number_of_orbital_transfers(orbit_map):
+    you_arr = []
+    cur_planet = orbit_map["YOU"]
+    while True:
+        you_arr.append(cur_planet)
+        try:
+            cur_planet = orbit_map[cur_planet]
+        except KeyError:
+            break
+
+    cur_planet, san_count = orbit_map["SAN"], 0
+    while True:
+        if cur_planet in you_arr:
+            return san_count + you_arr.index(cur_planet)
+        else:
+            try:
+                cur_planet = orbit_map[cur_planet]
+                san_count += 1  
+            except KeyError:
+                print("Error occurred!")
+                break
 
 
 def run_input_tests():
@@ -43,7 +65,7 @@ def run_input_tests():
         assert len(orbit) == 2
 
     orbits_2 = read_input(filename="Day06/day06_test.txt")
-    assert len(orbits_2) == 11
+    assert len(orbits_2) == 13
     for orbit in orbits_2:
         assert len(orbit) == 2
 
@@ -63,18 +85,27 @@ def run_orbit_counts_test():
     assert orbit_counts["L"] == 7
     
 def run_puzzle_solve_tests():
-    num_of_orbits = solve_puzzle(test=True)
+    num_of_orbits = find_number_of_orbits(test=True)
     assert num_of_orbits == 42
+
+def run_min_number_tests():
+    _, orbit_map = find_number_of_orbits(test=True)
+    min_number_of_orbits = find_min_number_of_orbital_transfers(orbit_map)
+    assert min_number_of_orbits == 4
 
 def run_tests():
     run_input_tests()
     run_orbit_map_tests()
     run_orbit_counts_test()
-    run_puzzle_solve_tests()
+    #run_puzzle_solve_tests()
+    run_min_number_tests()
 
     print("All tests passed")
 
 run_tests()
 
-number_of_orbits = solve_puzzle()
-print(f"The number of orbits is {number_of_orbits}.")
+number_of_orbits, orbit_map = find_number_of_orbits()
+print(f"The number of total orbits is {number_of_orbits}.")
+
+min_number_of_orbits = find_min_number_of_orbital_transfers(orbit_map)
+print(f"The minimum number of orbits I have to take is {min_number_of_orbits}.")
